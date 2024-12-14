b39595f6b504661eacb02f4fffa258f9624ba26a
Sergey <folp228@gmail.com>
2024-12-14 15:30:12 +0300

    fix

---
+++ old_ copy/run.sh
@@ -0,0 +1,52 @@
+#!/bin/bash
+
+# Enable exit on error
+set -e
+
+# Define your workspace directory
+WORKSPACE_DIR="$(pwd)"
+
+RUN_RVIZ=$1
+
+# Check if the workspace directory is correct
+if [ ! -d "$WORKSPACE_DIR/src" ]; then
+  echo "Error: '$WORKSPACE_DIR/src' directory does not exist!"
+  exit 1
+fi
+
+# Build the workspace using colcon
+echo "Building the workspace..."
+source /opt/ros/humble/setup.bash
+rosdep install -i --from-path src --rosdistro humble -y
+colcon build
+
+# Check if colcon build was successful before sourcing
+if [ $? -eq 0 ]; then
+  # Source the local setup file after building
+  source "install/local_setup.bash"
+else
+  echo "Error: colcon build failed!"
+  exit 1
+fi
+
+# Source the local setup file after building
+source "install/local_setup.bash"
+echo "Waiting for running launch file"
+
+# Setup camera driver
+modprobe -r uvcvideo
+modprobe uvcvideo nodrop=1 timeout=5000 quirks=0x80
+
+# run topics "image_raw" and "image_raw/image_compressed"
+ros2 run v4l2_camera v4l2_camera_node --ros-args -p image_size:="[640,480]"
+
+#Run example node
+
+if [ "$RUN_RVIZ" = "False" ]; then
+  # TODO Python and RViz cant use camera at the same time
+  python3 /camera_test/camera_test.py && ros2 launch camera camera_launch.launch.py rviz:=false
+else
+  python3 /camera_test/camera_test.py && ros2 launch camera camera_launch.launch.py
+fi
+ 
+
